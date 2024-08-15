4614be68eb4a3925178f3f22efb669069f7a9f27
hliu <hlzh00@icloud.com>
2024-08-15 12:52:34 -0700

    changed PORT

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ const generateId = () => {
     return String(maxId + 1)
 }
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log('Server running on ' + PORT)
 })
